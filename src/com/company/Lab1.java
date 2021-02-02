b16061d794a359f7fb291a261b60eaf12dae352a
BlackAkuma <76155737+BlackAkuma@users.noreply.github.com>
2021-02-02 11:40:00 +0200

    Add files via upload

---
+++ src/com/company/Lab1.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Lab1 {
+    public static void main(String[] args) {
+//        Номер залікової книги 331
+//        int C2 = 1; (О1 = -)
+//        int C3 = 1;
+//        int C5 = 1; (О2 = /)
+//        int C7 = 2; (Тип = int)
+
+        final int C = 1;
+        int a = 2;
+        int b = 1;
+        int n = 3;
+        int m = 3;
+        double res = 0;
+        for (int i = a; i <= n; i++) {
+            for (int j = b; j <= m; j++) {
+                res = res + ((double)(i/j)/(i-C));
+            }
+        }
+        System.out.println(res);
+    }
+}

04dfb082fc2c712f5f3ea178be85a944ba9f548b
BlackAkuma <76155737+BlackAkuma@users.noreply.github.com>
2021-02-03 16:47:25 +0200

    Update Lab1.java

--- src/com/company/Lab1.java
+++ src/com/company/Lab1.java
@@ -11,36 +11,40 @@ import java.util.Scanner;
  */
 public class Lab1 {
     public static void main(String[] args) {
+
         final int C = 1;
         double S = 0;
+        int temp = 0;
 
         Scanner sum = new Scanner(System.in);
-
         System.out.print("Введіть число a: ");
         int a = sum.nextInt();
-
         System.out.print("Введіть число n: ");
         int n = sum.nextInt();
-
         System.out.print("Введіть число b: ");
         int b = sum.nextInt();
-
         System.out.print("Введіть число m: ");
         int m = sum.nextInt();
 
-        if (a <= 1 || b <= 0 ) {
+        if ((a <= 1 && n > 0) || (b <= 0 && m >= 0)) {
             System.out.println("В одному з кроків відбувається ділення на 0.");
             System.exit(0);
+        } else if (a > n) {
+            temp = a;
+            a = n;
+            n = temp;
         }
-        else if ( a > n || b > m){
-            System.out.println("Число a(b) повинно бути менше рівне n(m). Запускайте програму знову.");
-            System.exit(0);
+        if (b > m) {
+            temp = b;
+            b = m;
+            m = temp;
         }
+
         for (int i = a; i <= n; i++) {
             for (int j = b; j <= m; j++) {
                 S += (((double) i / (double) j) / (i - C));
             }
         }
-        System.out.printf("Результат: %.2f.", S);
+        System.out.printf("***********************\n*                     *\n* Результат: %.2f *\n*                     *\n***********************", S);
     }
 }
